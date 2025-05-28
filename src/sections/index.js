3f53f08d178dda8d5a81294c43b21218b3e33b56
ADITYA_DR <raybandarkaraditya@gmail.com>
2025-05-28 15:51:24 +0530

    Nike Landing page

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+// import CustomerReviews from "./sections/CustomerReviews";
+// import Footer from "./sections/Footer";
+// import Hero from "./sections/Hero";
+// import PopularProducts from "./sections/PopularProducts";
+// import Services from "./sections/Services";
+// import SpecialOffer from "./sections/SpecialOffer";
+// import Subscribe from "./sections/Subscribe";
+// import SuperQuality from "./sections/SuperQuality";
+
+// export {
+//     CustomerReviews,
+//     Footer,
+//     Hero,
+//     PopularProducts,
+//     Services,
+//     SpecialOffer,
+//     Subscribe,
+//     SuperQuality
+// }
